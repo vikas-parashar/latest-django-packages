46920f541aafd8db30469a0f55660adfa1d21c75
vikas-parashar <svnitvikas@gmail.com>
2015-08-28 17:41:11 +0530

    sending mail ability added

--- scr.py
+++ scr.py
@@ -1,71 +1,71 @@
+# -- coding: utf-8 --
 from bs4 import BeautifulSoup
 import re
 import requests
 import urllib3
 
+soup = BeautifulSoup(open("pypi.html"), "html.parser")
 
-def difference(f1, f2):
-	with open(f1) as f:
-	    t1 = f.read().splitlines()
-	    t1s = set(t1)
+old = 'old.txt'
+new = 'new.txt'
+package_url_list = []
+mail_content_list = []
 
-	with open(f2) as f:
-	    t2 = f.read().splitlines()
-	    t2s = set(t2)
+def difference():
+    with open(old) as f:
+        t1 = f.read().splitlines()
+        t1s = set(t1)
 
-	diff_list = []
-	for diff in t2s-t1s:
-		diff_list.append(diff)
-	return diff_list
+    with open(new) as f:
+        t2 = f.read().splitlines()
+        t2s = set(t2)
+
+    diff_list = []
+    for diff in t2s-t1s:
+        diff_list.append(diff)
+    return diff_list
 
 
 def package_finder():
-	thefile = open(new, 'w')
-	for row in soup.find_all('a'):
-		package_name = row.text
-		pattern = 'django-'
+    thefile = open(new, 'w')
+    for row in soup.find_all('a'):
+        package_name = row.text
+        pattern = 'django-'
 
-		for match in re.findall(pattern, package_name):
-			   thefile.write("%s\n" % package_name.encode('utf-8'))
-	# print "file created"
+        for match in re.findall(pattern, package_name):
+            thefile.write("%s\n" % package_name.encode('utf-8'))
+
 
 def list_finder():
-	# diffile = open("dif.txt", 'w')
-	for item in new_package_list:
-		for link in soup.find_all('a',href=True, text=item):
-			package_url_list.append(link['href'])
-			print link['href']
-
-def new_to_old(new, old):
-	with open(new) as f:
-		    with open(old, "w") as f1:
-		        for line in f:
-		            f1.write(line)
+    # diffile = open("dif.txt", 'w')
+    for item in new_package_list:
+        for link in soup.find_all('a', href=True, text=item):
+            package_url_list.append(link['href'])
+            return package_url_list
 
 
-old='old.txt'
-new='new.txt'
-package_url_list = []
-
-soup = BeautifulSoup(open("pypi.html"), "html.parser")
-
-package_finder()
-
-new_package_list = difference(old,new)
-
-list_finder()
-
-new_to_old(new, old)
+def new_to_old():
+    with open(new) as f:
+        with open(old, "w") as f1:
+            for line in f:
+                f1.write(line)
 
 
-for el in package_url_list:
-	r = requests.get(el)
-	data = r.text
-	new_soup = BeautifulSoup(data, "html.parser")
+def mail_content():
+    for el in package_url_list:
+        r = requests.get(el)
+        data = r.text
+        new_soup = BeautifulSoup(data, "html.parser")
 
-	para = new_soup.find('div', class_='section').find('p')
-	print new_package_list[package_url_list.index(el)]
-	print para.text
-	print el
-	print "\n"
+        para = new_soup.find('div', class_='section').find('p')
+        name = new_package_list[package_url_list.index(el)].decode('utf-8')
+        description = para.text
+        description.decode('utf-8')
+        pypi_url = el.decode('utf-8')
 
+        message = name+"\n"+description+"\n"+pypi_url
+        mail_content_list.append(message)
+
+        return mail_content_list
+
+new_package_list = difference()
